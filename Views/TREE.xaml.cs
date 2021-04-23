0963da327e2797b75108bd072539661f4f9b13c4
DamianMateus <wilmer.damian@321ignition.com>
2021-04-23 10:02:33 -0500

    Add Save function

--- Views/TREE.xaml.cs
+++ Views/TREE.xaml.cs
@@ -7,9 +7,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace TestUwp.Views
 {
-    // For more info about the TreeView Control see
-    // https://docs.microsoft.com/windows/uwp/design/controls-and-patterns/tree-view
-    // For other samples, get the XAML Controls Gallery app http://aka.ms/XamlControlsGallery
+
     public sealed partial class TREE : Page
     {
         public TREEViewModel ViewModel { get; } = new TREEViewModel();
